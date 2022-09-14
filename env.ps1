fd1ef099a89b66a9a7d04d0f5de7b659071eb5d0
Ramya Gangula <ramya1331@gmail.com>
2022-09-14 14:03:44 -0500

    Update env.ps1

--- env.ps1
+++ env.ps1
@@ -1,4 +1,4 @@
-$Env:DBHOST = "localhost"
-$Env:DBUSER = "manager"
-$Env:DBNAME = "pollsdb"
-$Env:DBPASS = "supersecretpass"
+$Env:DBHOST = "djangoghpgdb"
+$Env:DBUSER = "username"
+$Env:DBNAME = "djangoghpgdb"
+$Env:DBPASS = "Password1"
